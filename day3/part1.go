fbfebd394d13e131b3bbe01318385bdee0cdc3e1
Kevin <vargaskevv@gmail.com>
2023-12-03 07:43:23 -0400

    Day 3, part 1

---
+++ day3/part1.go
@@ -0,0 +1,103 @@
+package day3
+
+import (
+	"strconv"
+	"unicode"
+
+	"github.com/jqvk/aoc2023/common"
+)
+
+type Coord struct {
+	row int
+	col int
+}
+
+func parseInput() [][]rune {
+	// lines := common.ReadFile("day3/input_small")
+	lines := common.ReadFile("day3/input")
+	var matrix [][]rune
+	for _, line := range lines {
+		var row []rune
+		for _, char := range line {
+			row = append(row, char)
+		}
+		matrix = append(matrix, row)
+	}
+	return matrix
+}
+
+func getCoordsToCheck(rowIndex, from, to int, matrix [][]rune) []Coord {
+	maxRow, maxCol := len(matrix)-1, len(matrix[0])-1
+	var initialList []Coord
+	// above and botton row
+	for col := from - 1; col <= to+1; col++ {
+		initialList = append(
+			initialList,
+			Coord{row: rowIndex - 1, col: col},
+			Coord{row: rowIndex + 1, col: col},
+		)
+	}
+	initialList = append(initialList, Coord{row: rowIndex, col: from - 1}, Coord{row: rowIndex, col: to + 1})
+	var cleanList []Coord
+	for _, coord := range initialList {
+		row, col := coord.row, coord.col
+		if row >= 0 && row <= maxRow && col >= 0 && col <= maxCol {
+			cleanList = append(cleanList, coord)
+		}
+	}
+	return cleanList
+}
+
+func containsSpecialChars(coords []Coord, matrix [][]rune) bool {
+	for _, coord := range coords {
+		char := matrix[coord.row][coord.col]
+		if char != '.' && !unicode.IsDigit(char) {
+			return true
+		}
+	}
+	return false
+}
+
+func calculatePartsSum(matrix [][]rune) int {
+	var sum, start, end int
+	var number string
+	var parsingNumber bool
+	for i, row := range matrix {
+		start, end = 0, 0
+		number, parsingNumber = "", false
+		for j, char := range row {
+			if unicode.IsDigit(char) {
+				number += string(char)
+			}
+			if unicode.IsDigit(char) && !parsingNumber {
+				parsingNumber = true
+				start = j
+			}
+			if !unicode.IsDigit(char) && parsingNumber {
+				end = j - 1
+				// check
+				coordsToCheck := getCoordsToCheck(i, start, end, matrix)
+				if containsSpecialChars(coordsToCheck, matrix) {
+					n, err := strconv.Atoi(number)
+					common.ErrPanic(err)
+					sum += n
+				}
+				number, start, end, parsingNumber = "", 0, 0, false
+			}
+		}
+		if parsingNumber {
+			coordsToCheck := getCoordsToCheck(i, start, len(row)-1, matrix)
+			if containsSpecialChars(coordsToCheck, matrix) {
+				n, err := strconv.Atoi(number)
+				common.ErrPanic(err)
+				sum += n
+			}
+		}
+	}
+	return sum
+}
+
+func Part1() int {
+	matrix := parseInput()
+	return calculatePartsSum(matrix)
+}
--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/jqvk/aoc2023/day1"
-	"github.com/jqvk/aoc2023/day2"
+	// j"github.com/jqvk/aoc2023/day1"
+	// "github.com/jqvk/aoc2023/day2"
+	"github.com/jqvk/aoc2023/day3"
 )
 
 func measureAndPrint[T any](label string, fn func() T) {
@@ -17,8 +18,9 @@ func measureAndPrint[T any](label string, fn func() T) {
 }
 
 func main() {
-	measureAndPrint("D1P1", day1.Part1)
-	measureAndPrint("D1P2", day1.Part2)
-	measureAndPrint("D2P1", day2.Part1)
-	measureAndPrint("D2P2", day2.Part2)
+	// measureAndPrint("D1P1", day1.Part1)
+	// measureAndPrint("D1P2", day1.Part2)
+	// measureAndPrint("D2P1", day2.Part1)
+	// measureAndPrint("D2P2", day2.Part2)
+	measureAndPrint("D3P1", day3.Part1)
 }
