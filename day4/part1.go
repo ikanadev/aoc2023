9020507a213b4ef3c217227a097c12ece2a0ba52
Kevin <vargaskevv@gmail.com>
2023-12-04 04:04:30 -0400

    Day 4, part 1

---
+++ day4/part1.go
@@ -0,0 +1,70 @@
+package day4
+
+import (
+	"math"
+	"strconv"
+	"strings"
+
+	"github.com/jqvk/aoc2023/common"
+)
+
+type Set struct {
+	id          int
+	winners     []int
+	cards       []int
+	winnerCards int
+}
+
+func (s *Set) calculateWinnerCards() {
+	winerCards := 0
+	for _, card := range s.cards {
+		for _, winner := range s.winners {
+			if card == winner {
+				winerCards++
+				break
+			}
+		}
+	}
+	s.winnerCards = winerCards
+}
+
+func parseInput() []Set {
+	// lines := common.ReadFile("day4/input_small")
+	lines := common.ReadFile("day4/input")
+	var sets []Set
+	for _, line := range lines {
+		var set Set
+		split := strings.Split(line, ":")
+		setSplit := strings.Fields(split[0])
+		cardsSplit := strings.Split(split[1], "|")
+		id, err := strconv.Atoi(setSplit[1])
+		common.ErrPanic(err)
+		set.id = id
+		winners := strings.Fields(strings.TrimSpace(cardsSplit[0]))
+		cards := strings.Fields(strings.TrimSpace(cardsSplit[1]))
+		for _, str := range winners {
+			n, err := strconv.Atoi(str)
+			common.ErrPanic(err)
+			set.winners = append(set.winners, n)
+		}
+		for _, str := range cards {
+			n, err := strconv.Atoi(str)
+			common.ErrPanic(err)
+			set.cards = append(set.cards, n)
+		}
+		sets = append(sets, set)
+	}
+	return sets
+}
+
+func Part1() int {
+	sets := parseInput()
+	totalPoints := 0
+	for i, _ := range sets {
+		sets[i].calculateWinnerCards()
+		if sets[i].winnerCards > 0 {
+			totalPoints += int(math.Pow(2, float64(sets[i].winnerCards-1)))
+		}
+	}
+	return totalPoints
+}
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jqvk/aoc2023/day1"
 	"github.com/jqvk/aoc2023/day2"
 	"github.com/jqvk/aoc2023/day3"
+	"github.com/jqvk/aoc2023/day4"
 )
 
 func measureAndPrint[T any](label string, fn func() T) {
@@ -24,4 +25,5 @@ func main() {
 	measureAndPrint("D2P2", day2.Part2)
 	measureAndPrint("D3P1", day3.Part1)
 	measureAndPrint("D3P2", day3.Part2)
+	measureAndPrint("D4P1", day4.Part1)
 }
